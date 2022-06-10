a38464dbedff32a9b97345c59bd2b4c695cae99c
Lairon Souza <laionconectado@gmail.com>
2022-06-10 12:45:44 -0300

    Adcionando comentários ao código para facilitar o entendimento

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import speech_recognition as sr
 
-rec = sr.Recognizer()
-# print(sr.Microphone().list_microphone_names())
-with sr.Microphone() as mic:
-	rec.adjust_for_ambient_noise(mic)
-	print("Gravando audio...")
-	audio = rec.listen(mic)
-	texto = rec.recognize_google(audio, language="pt-BR")
-	print(texto)
+rec = sr.Recognizer() # INSTANCIANDO O RECONHECEDOR DE VOZ
 
+# print(sr.Microphone().list_microphone_names()) --> AQUI PRINTAMOS NO CONSOLE A LISTA DE MICROFONES CONECTADOS QUE QUEREMOS USAR
+
+with sr.Microphone() as mic: # CONTROLANDO O MICROFONE
+	rec.adjust_for_ambient_noise(mic) # INDINCANDO QUAL TIPO DE SOM DESEJAMOS CAPTURAR - NO NOSSO CASO O (mic)
+	print("Gravando audio...") # PRINTA UMA MENSAGEM ANTES DE GRAVAR
+	audio = rec.listen(mic) # OUVINDO O AUDIO 
+	texto = rec.recognize_google(audio, language="pt-BR") # AQUI ESCOLHEMOS QUAL RECONHECEDOR VAMOS USAR, NESTE CASO O DO GOOGLE, E DEFINIMOS A LÍNGUA TBM
+	print(texto) # POR FIM PRINTAMOS O QUE FOI DITO NO CONSOLE
